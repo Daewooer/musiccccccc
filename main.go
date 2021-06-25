89a6835f056750d4a3e643003f928a382ad6a666
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-06-25 12:50:44 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ηello World")
+	fmt.Println("What is your favourite song")
+	var SongInput
+
+	fmt.Scanln(&first)
+	fmt.Println("What is your favourite band?")
+	var  BandInput
+	fmt.Scanln("&second")
+	fmt.Println("thnx for the info")
+}
